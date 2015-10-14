9972541128439f02d371d482726655936aa156c8
Scott Mitchell <smitchell@pubint.com>
2015-10-14 14:47:44 -0500

    more index.js

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var app = express();
 
 app.set('port', (process.env.PORT || 8000));
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(request, response) {
     response.send("Welcome to the Music MMO!");
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
