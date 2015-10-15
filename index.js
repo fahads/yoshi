462dac604cd174650c923cf8d1efc0fa96757a6e
Scott <scott7mitchell@gmail.com>
2015-10-14 19:36:47 -0500

    Add names

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 8100));
+app.set('port', (process.env.PORT || 1337));
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-    response.send("Welcome to the Music MMO!");
+    response.send("Welcome to the Music MMO! By: Fahad, Mark, and Scott.");
 });
 
 app.listen(app.get('port'), function() {
