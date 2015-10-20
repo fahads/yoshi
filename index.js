38faf67312f367cc230d6ca7e87e58db804c5754
Scott Mitchell <smitchell@pubint.com>
2015-10-20 11:13:26 -0500

    Cleanup

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ app.set('port', (process.env.PORT || 1337));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-<<<<<<< HEAD
-    response.send("Welcome to the Music MMO! By: Fahad, Mark, and Scott.");
-=======
-    response.send("Welcome to the Music MMO! (C) Northwestern University. Fahad, Mark, and Scott.");
->>>>>>> 9025598bb5b799a3437e5a04cc6f0975c2414f41
+response.send("Welcome to the Music MMO! (C) Northwestern University. Fahad, Mark, and Scott.");
 });
 
 app.listen(app.get('port'), function() {
