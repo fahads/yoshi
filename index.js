3df56da2b9e0e086b2370476ce589c902377f60d
Scott <scott7mitchell@gmail.com>
2015-11-02 14:42:05 -0600

    ang

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 1337));
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/css'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
