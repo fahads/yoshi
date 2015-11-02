42c59880702e2d89038be04aa3498650b7f3e22c
Scott <scott7mitchell@gmail.com>
2015-11-02 13:28:24 -0600

    First angular stuff

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ app.set('port', (process.env.PORT || 1337));
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-response.send("Welcome to the Music MMO! (C) Northwestern University. Fahad, Mark, and Scott.");
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
 });
+
+//app.get('/', function(request, response) {
+//response.send("Welcome to the Music MMO! (C) Northwestern University. Fahad, Mark, and Scott.");
+//});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
