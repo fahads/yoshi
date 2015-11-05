feaa3c91e87cd351ac55b77f4ee0b8c3ba3272ce
Fahad Sami <fahad.sami@gmail.com>
2015-11-05 08:33:51 -0600

    store session cookie, make sure correct data is being passed to template

--- routes/index.js
+++ routes/index.js
@@ -6,28 +6,42 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Musicador' });
 });
 
+// new session route and render
 router.get('/new-session', function(req, res) {
 	res.render('new-session', { title: "New Jam Session "});
 });
 
+// post request to create new session from form
 router.post('/create-session', function(req, res) {
 	var db = req.db;
-	var instrument = req.body.instrument;
+	// var instrument = req.body.instrument;
 	var username = req.body.username;
-	var jamName = req.body.jam-name;
+	var jamname = req.body.jamname;
 	var session = db.get('sessions');
 
 	session.insert({
 		"username" : username,
-		"jam" : jamName,
-		"instrument" : instrument
+		"jam" : jamname,
+		// "instrument" : instrument
 	}, function(err, doc) {
 		if (err) {
 			res.send("Error cannot add data to db.");
 		} else {
+			res.cookie('session', doc._id);
 			res.redirect("new-session");
 		}
 	})
 })
+
+// view that shows list of sessions
+router.get('/sessionlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('sessions');
+    collection.find({},{},function(e,docs){
+        res.render('sessionlist', {
+            sessionlist : docs
+        });
+    });
+});
 
 module.exports = router;
