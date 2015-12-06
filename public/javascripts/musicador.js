ee8d2d87b6a0af0ab435654e09430a3b2c598005
Fahad Sami <fahad.sami@gmail.com>
2015-12-06 16:00:33 -0600

    adding socket.io support and implementation in new session view

--- public/javascripts/musicador.js
+++ public/javascripts/musicador.js
@@ -1,11 +1,16 @@
 var ctx = new AudioContext();
 var piano = null;
-var note = "C4"
 var time = ctx.currentTime
 soundfont(ctx, 'acoustic_grand_piano').then(function(instrument) {
   piano = instrument;
 });
 
+var socket = io();
+
 function playNote(note) {
-  piano.play(note, time);
+  socket.emit('played note', note);
 }
+
+socket.on('played note', function(note){
+  piano.play(note, time);
+});
